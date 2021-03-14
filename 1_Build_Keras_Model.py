a8889d7d53f095dac42642ae5e3cbcc282c2430c
malonso99 <mikealonso99@gmail.com>
2021-03-14 21:07:28 +0100

    Prediction added

--- 1_Build_Keras_Model.py
+++ 1_Build_Keras_Model.py
@@ -118,6 +118,124 @@ model.fit(
 # Neural Networks Predictions
 ##################
 
+test_labels =  []
+test_samples = []
+
+"""
+Same code as before but we now use it to generate the samples that we will predict from the trained model
+on file 1_Build_Keras_Model
+"""
+for i in range(10):
+    # The 5% of younger individuals who did experience side effects
+    random_younger = randint(13,64)
+    test_samples.append(random_younger)
+    test_labels.append(1)
+
+    # The 5% of older individuals who did not experience side effects
+    random_older = randint(65,100)
+    test_samples.append(random_older)
+    test_labels.append(0)
+
+for i in range(200):
+    # The 95% of younger individuals who did not experience side effects
+    random_younger = randint(13,64)
+    test_samples.append(random_younger)
+    test_labels.append(0)
+
+    # The 95% of older individuals who did experience side effects
+    random_older = randint(65,100)
+    test_samples.append(random_older)
+    test_labels.append(1)
+
+test_labels = np.array(test_labels)
+test_samples = np.array(test_samples)
+test_labels, test_samples = shuffle(test_labels, test_samples)
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_test_samples = scaler.fit_transform(test_samples.reshape(-1,1))
+
+predictions = model.predict(
+     x=scaled_test_samples,
+     batch_size=10,
+     verbose=0
+)  
+for i in predictions:
+    print(i)  # 2D Output: the probability of experiencing a side effect and probability of not experiencing a side effect
+
+"""Check how if a pecient is predicted to have side effect or not"""
+rounded_predictions = np.argmax(predictions, axis=-1)
+
+for i in rounded_predictions:
+    print(i)
+
+########################################
+# Visualizing Results: Confussion Matrices
+########################################
+
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+
+cm = confusion_matrix(y_true=test_labels, y_pred=rounded_predictions)
+
+
+def plot_confusion_matrix(cm, classes,      
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.Blues):
+    """Function from scikit-learn website"""
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+
+cm_plot_labels = ['no_side_effects','had_side_effects']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
